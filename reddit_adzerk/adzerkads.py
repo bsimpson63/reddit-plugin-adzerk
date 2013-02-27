90d933a957509da4189b8f793e27ac563b73264b
Max Goodman <c@chromakode.com>
2013-02-27 15:37:27 -0800

    Use c.request_origin for frame origin.

--- reddit_adzerk/adzerkads.py
+++ reddit_adzerk/adzerkads.py
@@ -8,10 +8,8 @@ class Ads(BaseAds):
         BaseAds.__init__(self)
         adzerk_test_srs = g.live_config.get("adzerk_test_srs")
         if adzerk_test_srs and c.site.name in adzerk_test_srs:
-            if c.secure:
-                self.ad_url = g.config["adzerk_https_url"].format(
-                                origin=g.https_endpoint)
-            else:
-                self.ad_url = g.config["adzerk_url"].format(
-                                origin=g.origin)
+            url_key = "adzerk_https_url" if c.secure else "adzerk_url"
+            self.ad_url = g.config[url_key].format(
+                origin=c.request_origin,
+            )
             self.frame_id = "ad_main"
