424d8ed4b24afc8fc0c2e8e7fd9212091a5a0fcc
Neil Williams <neil@reddit.com>
2013-03-05 14:08:14 -0800

    Check for test subreddits case insensitively.

--- reddit_adzerk/adzerkads.py
+++ reddit_adzerk/adzerkads.py
@@ -9,7 +9,7 @@ class Ads(BaseAds):
     def __init__(self):
         BaseAds.__init__(self)
         adzerk_test_srs = g.live_config.get("adzerk_test_srs")
-        if adzerk_test_srs and c.site.name in adzerk_test_srs:
+        if adzerk_test_srs and c.site.name.lower() in adzerk_test_srs:
             url_key = "adzerk_https_url" if c.secure else "adzerk_url"
             self.ad_url = g.config[url_key].format(
                 subreddit=quote(c.site.name),
