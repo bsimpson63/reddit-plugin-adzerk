11ac44c41d9cd3431b7f2cc833656665c8a9947b
Neil Williams <neil@reddit.com>
2013-08-12 15:59:53 -0700

    Change frontpage keyword to "-reddit.com".

--- reddit_adzerk/adzerkads.py
+++ reddit_adzerk/adzerkads.py
@@ -3,6 +3,7 @@ from urllib import quote
 from pylons import c, g
 
 from r2.lib.pages import Ads as BaseAds
+from r2.models.subreddit import DefaultSR
 
 
 class Ads(BaseAds):
@@ -11,6 +12,11 @@ class Ads(BaseAds):
 
         url_key = "adzerk_https_url" if c.secure else "adzerk_url"
         site_name = getattr(c.site, "analytics_name", c.site.name)
+
+        # adzerk reporting is easier when not using a space in the tag
+        if isinstance(c.site, DefaultSR):
+            site_name = "-reddit.com"
+
         self.ad_url = g.config[url_key].format(
             subreddit=quote(site_name.lower()),
             origin=c.request_origin,
