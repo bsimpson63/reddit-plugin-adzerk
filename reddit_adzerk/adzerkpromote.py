ce3365898cc783cbbb8863f711a9e5f77fbc4cf3
Brian Simpson <bsimpson63@gmail.com>
2014-01-13 14:47:30 -0500

    adzerk_request: Do some logging to get details of bad responses.

--- reddit_adzerk/adzerkpromote.py
+++ reddit_adzerk/adzerkpromote.py
@@ -454,6 +454,8 @@ def adzerk_request(keywords, num_placements=1, timeout=1.5):
         response = adzerk_api.handle_response(r)
     except adzerk_api.AdzerkError:
         g.stats.simple_event('adzerk.request.badresponse')
+        g.log.error('adzerk_request: bad response (%s) %s', r.status_code,
+                    r.text)
         return None
 
     decisions = response['decisions']
