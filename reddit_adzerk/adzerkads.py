856bdb5219b233769dc2529772d8489c20fb1efc
David Wick <wick.d.spencer@gmail.com>
2015-10-09 12:22:56 -0700

    Use protocol relative adzerk url

--- reddit_adzerk/adzerkads.py
+++ reddit_adzerk/adzerkads.py
@@ -13,14 +13,13 @@ class Ads(BaseAds):
     def __init__(self):
         BaseAds.__init__(self)
 
-        url_key = "adzerk_https_url" if c.secure else "adzerk_url"
         site_name = getattr(c.site, "analytics_name", c.site.name)
 
         # adzerk reporting is easier when not using a space in the tag
         if isinstance(c.site, DefaultSR):
             site_name = FRONTPAGE_NAME
 
-        self.ad_url = g.config[url_key].format(
+        self.ad_url = g.adzerk_url.format(
             subreddit=quote(site_name.lower()),
             origin=c.request_origin,
             loggedin="loggedin" if c.user_is_loggedin else "loggedout",
