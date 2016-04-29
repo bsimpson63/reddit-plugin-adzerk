96856fc267ec99de6e83a997346c853dbdb1cfd5
James Maa <james.maa@reddit.com>
2016-04-29 13:57:06 -0700

    Fix adhoc multisubreddit promo_request network request

--- reddit_adzerk/lib/validator.py
+++ reddit_adzerk/lib/validator.py
@@ -8,9 +8,11 @@ from r2.lib.validator import (
 from r2.models import (
     NotFound,
     Subreddit,
+    MultiReddit,
 )
 
 is_multi_rx = re.compile(r"\A/?(user|r)/[^\/]+/m/(?P<name>.*?)/?\Z")
+is_adhoc_multi_rx = re.compile(r"\A\/r\/((?:[0-z]+\+)+(?:[0-z])+)\Z")
 
 class VSite(Validator):
     def __init__(self, param, required=True, *args, **kwargs):
@@ -20,9 +22,13 @@ class VSite(Validator):
     def run(self, path):
         if not self.required and not path:
             return
-
+        adhoc_multi_rx = is_adhoc_multi_rx.match(path)
         if is_multi_rx.match(path):
             return VMultiByPath(self.param, kinds=("m")).run(path)
+        elif adhoc_multi_rx:
+            sr_strings = adhoc_multi_rx.groups()[0].split("+")
+            srs = Subreddit._by_name(sr_strings, stale=True).values()
+            return MultiReddit(path, srs)
         else:
             try:
                 return Subreddit._by_name(path)
